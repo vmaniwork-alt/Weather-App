3779da5dc6640d38b2abad2cb134d3fd08873080
Mani V <vmani.work@gmail.com>
2025-12-17 18:16:02 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Weather from './components/Weather'
+import React from "react";
+import Weather from "./components/Weather";
 
 function App() {
- 
-
   return (
     <>
-     <Weather/>
+      <Weather />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
